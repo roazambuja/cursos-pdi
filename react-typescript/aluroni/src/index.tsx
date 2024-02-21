e8357005941dff2e809ab97993f9f86e980ff8b8
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-02-21 11:43:16 -0300

    feat: import font and use normalize.css

--- react-typescript/aluroni/src/index.tsx
+++ react-typescript/aluroni/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Cardapio from "./pages/Cardapio";
+import "normalize.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -9,5 +10,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Cardapio />
+    Aluroni
   </React.StrictMode>
 );
