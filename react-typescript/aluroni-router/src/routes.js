2487c046e6fe9cbb341036ae912d125c1d18d6ea
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-03-06 16:33:14 -0300

    feat: details page with useLocation

--- react-typescript/aluroni-router/src/routes.js
+++ react-typescript/aluroni-router/src/routes.js
@@ -4,6 +4,7 @@ import PaginaPadrao from 'components/PaginaPadrao';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import NotFound from 'pages/NotFound';
+import Prato from 'pages/Prato';
 import Sobre from 'pages/Sobre';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
@@ -17,8 +18,9 @@ export default function AppRouter(){
                         <Route index element={<Inicio />} />
                         <Route path='cardapio' element={<Cardapio />} />
                         <Route path='sobre' element={<Sobre />} />
-                        <Route path='*' element={<NotFound />} />
                     </Route>
+                    <Route path='*' element={<NotFound />} />
+                    <Route path='prato/:id' element={<Prato />} />
                 </Routes>
                 <Footer />
             </Router>
