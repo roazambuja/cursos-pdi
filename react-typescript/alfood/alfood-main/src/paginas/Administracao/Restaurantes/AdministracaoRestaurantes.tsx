a0618eb2dbc75a8eab14e9c4aea39800f3431b6e
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-03-12 16:51:16 -0300

    feat: update and delete restaurants

--- react-typescript/alfood/alfood-main/src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ react-typescript/alfood/alfood-main/src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -6,10 +6,12 @@ import {
   TableRow,
   TableCell,
   TableBody,
+  Button,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import IRestaurante from "../../../interfaces/IRestaurante";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const AdministracaoRestaurantes = () => {
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
@@ -20,18 +22,45 @@ const AdministracaoRestaurantes = () => {
       .then((resposta) => setRestaurantes(resposta.data));
   });
 
+  const excluir = (restauranteASerExcluido: IRestaurante) => {
+    axios
+      .delete(
+        `http://localhost:8000/api/v2/restaurantes/${restauranteASerExcluido.id}/`
+      )
+      .then(() => {
+        const listaRestaurante = restaurantes.filter(
+          (restaurante) => restaurante.id !== restauranteASerExcluido.id
+        );
+        setRestaurantes([...listaRestaurante]);
+      });
+  };
+
   return (
     <TableContainer component={Paper}>
       <Table>
         <TableHead>
           <TableRow>
             <TableCell>Nome</TableCell>
+            <TableCell>Editar</TableCell>
+            <TableCell>Excluir</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {restaurantes.map((restaurante) => (
             <TableRow key={restaurante.id}>
               <TableCell>{restaurante.nome}</TableCell>
+              <TableCell>
+                <Link to={`/admin/restaurantes/${restaurante.id}`}>editar</Link>
+              </TableCell>
+              <TableCell>
+                <Button
+                  variant="outlined"
+                  color="error"
+                  onClick={() => excluir(restaurante)}
+                >
+                  Excluir
+                </Button>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
--- react-typescript/alfood/alfood-main/src/paginas/Administracao/Restaurantes/FormularioRestaurantes.tsx
+++ react-typescript/alfood/alfood-main/src/paginas/Administracao/Restaurantes/FormularioRestaurantes.tsx
@@ -1,28 +1,70 @@
-import { Button, TextField } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import IRestaurante from "../../../interfaces/IRestaurante";
 import axios from "axios";
-import React, { useState } from "react";
 
 const FormularioRestaurante = () => {
+  const parametros = useParams();
+
+  useEffect(() => {
+    if (parametros.id) {
+      axios
+        .get<IRestaurante>(`restaurantes/${parametros.id}/`)
+        .then((resposta) => setNomeRestaurante(resposta.data.nome));
+    }
+  }, [parametros]);
+
   const [nomeRestaurante, setNomeRestaurante] = useState("");
 
   const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
     evento.preventDefault();
-    
-    axios.post('http://localhost:8000/api/v2/restaurantes/', { nome: nomeRestaurante})
-    .then(resposta => console.log(resposta));
+
+    if (parametros.id) {
+      axios
+        .put(`restaurantes/${parametros.id}/`, {
+          nome: nomeRestaurante,
+        })
+        .then(() => {
+          alert("Restaurante atualizado com sucesso!");
+        });
+    } else {
+      axios
+        .post("restaurantes/", {
+          nome: nomeRestaurante,
+        })
+        .then(() => {
+          alert("Restaurante cadastrado com sucesso!");
+        });
+    }
   };
 
   return (
-    <form onSubmit={aoSubmeterForm}>
-      <TextField
-        value={nomeRestaurante}
-        onChange={(evento) => setNomeRestaurante(evento.target.value)}
-        id="standard-basic"
-        label="Nome do restaurante"
-        variant="standard"
-      />
-      <Button type="submit" variant="outlined">Enviar</Button>
-    </form>
+    <Box
+      sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <Typography component="h1" variant="h6">
+        Formulário de Restaurantes
+      </Typography>
+      <Box component="form" onSubmit={aoSubmeterForm}>
+        <TextField
+          value={nomeRestaurante}
+          onChange={(evento) => setNomeRestaurante(evento.target.value)}
+          label="Nome do Restaurante"
+          variant="standard"
+          fullWidth
+          required
+        />
+        <Button
+          sx={{ marginTop: 1 }}
+          type="submit"
+          fullWidth
+          variant="outlined"
+        >
+          Salvar
+        </Button>
+      </Box>
+    </Box>
   );
 };
 
