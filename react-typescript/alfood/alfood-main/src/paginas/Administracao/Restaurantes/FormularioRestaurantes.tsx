edb7710212889e184b35d6ca4d80880c3f5716f5
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-03-14 12:47:12 -0300

    feat: remove appbar from form

--- react-typescript/alfood/alfood-main/src/paginas/Administracao/Restaurantes/FormularioRestaurantes.tsx
+++ react-typescript/alfood/alfood-main/src/paginas/Administracao/Restaurantes/FormularioRestaurantes.tsx
@@ -51,22 +51,6 @@ const FormularioRestaurante = () => {
 
   return (
     <>
-      <AppBar position="static">
-        <Container maxWidth="xl">
-          <Toolbar>
-            <Typography variant="h6">Administração</Typography>
-            <Box sx={{ display: "flex", flexGrow: 1 }}>
-              <Link component={RouterLink} to="/admin/restaurantes">
-                <Button sx={{ my: 2, color: "white" }}>Restaurantes</Button>
-              </Link>
-              <Link component={RouterLink} to="/admin/restaurantes/novo">
-                <Button sx={{ my: 2, color: "white" }}>Novo restaurante</Button>
-              </Link>
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
-
       <Box>
         <Container maxWidth="lg" sx={{ mt: 1 }}>
           <Paper sx={{ p: 2 }}>
