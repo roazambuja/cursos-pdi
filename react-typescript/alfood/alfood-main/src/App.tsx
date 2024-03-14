3d17d72ca2466b1f55ecf6cc43f958d8e4c3c304
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-03-14 11:28:36 -0300

    feat: admin layout and routes

--- react-typescript/alfood/alfood-main/src/App.tsx
+++ react-typescript/alfood/alfood-main/src/App.tsx
@@ -1,8 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './paginas/Home';
-import VitrineRestaurantes from './paginas/VitrineRestaurantes';
+import PaginaBaseAdmin from './paginas/Administracao/PaginaBaseAdmin';
 import AdministracaoRestaurantes from './paginas/Administracao/Restaurantes/AdministracaoRestaurantes';
 import FormularioRestaurante from './paginas/Administracao/Restaurantes/FormularioRestaurantes';
+import Home from './paginas/Home';
+import VitrineRestaurantes from './paginas/VitrineRestaurantes';
 
 function App() {
 
@@ -10,9 +11,11 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/restaurantes" element={<VitrineRestaurantes />} />
-      <Route path="/admin/restaurantes" element={<AdministracaoRestaurantes />} />
-      <Route path="/admin/restaurantes/novo" element={<FormularioRestaurante />} />
-      <Route path="/admin/restaurantes/:id" element={<FormularioRestaurante />} />
+      <Route path='/admin' element={<PaginaBaseAdmin />}>
+        <Route path="restaurantes" element={<AdministracaoRestaurantes />} />
+        <Route path="restaurantes/novo" element={<FormularioRestaurante />} />
+        <Route path="restaurantes/:id" element={<FormularioRestaurante />} />
+      </Route>
     </Routes>
   );
 }
