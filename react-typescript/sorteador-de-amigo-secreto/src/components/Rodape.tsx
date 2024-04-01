829171918945221c7c852e63a748298cc39bf94f
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-04-01 13:34:31 -0300

    feat: rodape component

---
+++ react-typescript/sorteador-de-amigo-secreto/src/components/Rodape.tsx
@@ -0,0 +1,14 @@
+import { useListaDeParticipantes } from "../state/hook/useListaDeParticipantes";
+
+const Rodape = () => {
+
+    const participantes = useListaDeParticipantes();
+
+    return (
+        <footer>
+            <button disabled={participantes.length < 3}>Iniciar brincadeira!</button>
+        </footer>
+    )
+}
+
+export default Rodape;
