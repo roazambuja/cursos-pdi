078496a71331212684396036104fa6e54746b8c1
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-04-01 14:49:29 -0300

    feat: configuration page

---
+++ react-typescript/sorteador-de-amigo-secreto/src/pages/Configuracao.tsx
@@ -0,0 +1,15 @@
+import Formulario from "../components/Formulario";
+import ListaParticipantes from "../components/ListaParticipantes";
+import Rodape from "../components/Rodape";
+
+const Configuracao = () => {
+    return (
+        <>
+            <Formulario />
+            <ListaParticipantes />
+            <Rodape />
+        </>
+    )
+}
+
+export default Configuracao;
