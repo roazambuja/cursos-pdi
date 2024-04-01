d34ad1a9ed327339c096434bf519098dc40e6c45
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-04-01 13:34:48 -0300

    test: rodape test

---
+++ react-typescript/sorteador-de-amigo-secreto/src/components/Rodape.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react"
+import { RecoilRoot } from "recoil"
+import Rodape from "./Rodape";
+
+describe('onde não existem participantes suficientes', () => {
+    test('a brincadeira não pode ser iniciada', () => {
+        render(
+            <RecoilRoot>
+                <Rodape />
+            </RecoilRoot>
+        );
+
+        const botao = screen.getByRole('button');
+        expect(botao).toBeDisabled();
+
+    })
+})
