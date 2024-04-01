ea3bd3210d2aace37c14e3d99748b2fc2de4c6e3
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-04-01 14:54:56 -0300

    fix: remove unused import

--- react-typescript/sorteador-de-amigo-secreto/src/App.tsx
+++ react-typescript/sorteador-de-amigo-secreto/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import Formulario from './components/Formulario';
