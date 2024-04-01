297342d418398ec659c0be180f4aea8712f19528
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-04-01 14:42:30 -0300

    feat: navigate to sorteio

--- react-typescript/sorteador-de-amigo-secreto/src/components/Rodape.tsx
+++ react-typescript/sorteador-de-amigo-secreto/src/components/Rodape.tsx
@@ -1,12 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import { useListaDeParticipantes } from "../state/hook/useListaDeParticipantes";
 
 const Rodape = () => {
 
     const participantes = useListaDeParticipantes();
+    const navegarPara = useNavigate();
+
+    const iniciar = () => {
+        navegarPara('/sorteio');
+    }
 
     return (
         <footer>
-            <button disabled={participantes.length < 3}>Iniciar brincadeira!</button>
+            <button disabled={participantes.length < 3} onClick={iniciar}>Iniciar brincadeira!</button>
         </footer>
     )
 }
