4c9d467623d53cbb1af5890a32881f247eb6be34
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-04-01 15:42:53 -0300

    feat: style pages and components

--- react-typescript/sorteador-de-amigo-secreto/src/components/Rodape.tsx
+++ react-typescript/sorteador-de-amigo-secreto/src/components/Rodape.tsx
@@ -1,20 +1,22 @@
-import { useNavigate } from "react-router-dom";
-import { useListaDeParticipantes } from "../state/hook/useListaDeParticipantes";
+import { useNavigate } from "react-router-dom"
+import { useListaDeParticipantes } from "../state/hook/useListaDeParticipantes"
+
+import './Rodape.css'
 
 const Rodape = () => {
 
-    const participantes = useListaDeParticipantes();
-    const navegarPara = useNavigate();
+    const participantes = useListaDeParticipantes()
+
+    const navegarPara = useNavigate()
 
     const iniciar = () => {
-        navegarPara('/sorteio');
+        navegarPara('/sorteio')
     }
 
-    return (
-        <footer>
-            <button disabled={participantes.length < 3} onClick={iniciar}>Iniciar brincadeira!</button>
-        </footer>
-    )
+    return (<footer className="rodape-configuracoes">
+        <button className="botao" disabled={participantes.length < 3} onClick={iniciar}>Iniciar brincadeira</button>
+        <img src="/imagens/sacolas.png" alt="Sacolas de compras" />
+    </footer>)
 }
 
-export default Rodape;
+export default Rodape
--- react-typescript/sorteador-de-amigo-secreto/src/pages/Configuracao.tsx
+++ react-typescript/sorteador-de-amigo-secreto/src/pages/Configuracao.tsx
@@ -1,15 +1,19 @@
-import Formulario from "../components/Formulario";
-import ListaParticipantes from "../components/ListaParticipantes";
-import Rodape from "../components/Rodape";
+import Card from "../components/Card"
+import Formulario from "../components/Formulario"
+import ListaParticipantes from "../components/ListaParticipantes"
+import Rodape from "../components/Rodape"
 
 const Configuracao = () => {
     return (
-        <>
-            <Formulario />
-            <ListaParticipantes />
-            <Rodape />
-        </>
+        <Card>
+            <section>
+                <h2>Vamos começar!</h2>
+                <Formulario />
+                <ListaParticipantes />
+                <Rodape />
+            </section>
+        </Card>
     )
 }
 
-export default Configuracao;
+export default Configuracao
