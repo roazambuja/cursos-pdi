88aed40411228bc66d7c8fc42e1bb995e77011a5
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-04-02 10:13:56 -0300

    feat: draw participants

--- react-typescript/sorteador-de-amigo-secreto/src/components/Rodape.tsx
+++ react-typescript/sorteador-de-amigo-secreto/src/components/Rodape.tsx
@@ -2,15 +2,19 @@ import { useNavigate } from "react-router-dom"
 import { useListaDeParticipantes } from "../state/hook/useListaDeParticipantes"
 
 import './Rodape.css'
+import { useSorteador } from "../state/hook/useSorteador"
 
 const Rodape = () => {
 
-    const participantes = useListaDeParticipantes()
+    const participantes = useListaDeParticipantes();
 
-    const navegarPara = useNavigate()
+    const navegarPara = useNavigate();
+
+    const sortear = useSorteador();
 
     const iniciar = () => {
-        navegarPara('/sorteio')
+        sortear();
+        navegarPara('/sorteio');
     }
 
     return (<footer className="rodape-configuracoes">
