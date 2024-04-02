45f158f0947f4f9ede7692a736e10e5d7104a201
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-04-02 15:18:54 -0300

    test: test if alert is hidden after 5 seconds

--- react-typescript/sorteador-de-amigo-secreto/src/pages/Sorteio.test.tsx
+++ react-typescript/sorteador-de-amigo-secreto/src/pages/Sorteio.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react"
+import { fireEvent, render, screen, act } from "@testing-library/react"
 import { RecoilRoot } from "recoil"
 import { useListaDeParticipantes } from "../state/hook/useListaDeParticipantes";
 import Sorteio from "./Sorteio";
@@ -64,5 +64,26 @@ describe('a pagina de sorteio', () => {
         fireEvent.click(botao);
         const amigoSecreto = screen.getByRole('alert');
         expect(amigoSecreto).toBeInTheDocument();
+    })
+
+    test('esconde o amigo secreto sorteado depois de 5 segundos', async () => {
+        jest.useFakeTimers();
+
+        render(
+            <RecoilRoot>
+                <Sorteio />
+            </RecoilRoot>
+        )
+
+        const select = screen.getByPlaceholderText('Selecione o seu nome')
+        fireEvent.change(select, { target: { value: participantes[1] } })
+
+        const button = screen.getByRole('button')
+        fireEvent.click(button)
+        act(() => {
+            jest.runAllTimers();
+        })
+        const alerta = screen.queryByRole('alert')
+        expect(alerta).not.toBeInTheDocument()
     })
 })
