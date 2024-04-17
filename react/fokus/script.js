0dc674f31c6ee66ae35a27dd5a66ab3438c327ec
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-04-17 10:18:14 -0300

    feat: change text on click

--- react/fokus/script.js
+++ react/fokus/script.js
@@ -4,34 +4,39 @@ const curtoBt = document.querySelector(".app__card-button--curto");
 const longoBt = document.querySelector(".app__card-button--longo");
 const banner = document.querySelector(".app__image");
 const titulo = document.querySelector(".app__title");
+const botoes = document.querySelectorAll(".app__card-button");
 
-focoBt.addEventListener('click', () => {
-    alterarContexto('foco');
+focoBt.addEventListener("click", () => {
+  alterarContexto("foco");
+  focoBt.classList.add("active");
 });
 
-curtoBt.addEventListener('click', () => {
-    alterarContexto('descanso-curto');
-
+curtoBt.addEventListener("click", () => {
+  alterarContexto("descanso-curto");
+  curtoBt.classList.add("active");
 });
 
-longoBt.addEventListener('click', () => {
-    alterarContexto('descanso-longo');
-
+longoBt.addEventListener("click", () => {
+  alterarContexto("descanso-longo");
+  longoBt.classList.add("active");
 });
 
 function alterarContexto(contexto) {
-    html.setAttribute('data-contexto', contexto);
-    banner.setAttribute('src', `./imagens/${contexto}.png`);
-    switch(contexto) {
-        case "foco":
-            titulo.innerHTML = `Otimize sua produtividade,<br><strong class="app__title-strong">mergulhe no que importa.</strong>`
-            break;
-        case "descanso-curto":
-            titulo.innerHTML = `Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>`
-            break;
-        case "descanso-longo": 
-            titulo.innerHTML = `Hora de voltar à superfície. <strong class="app__title-strong">Faça uma pausa longa.</strong>` 
-        default:
-            break;
-    }
+  botoes.forEach(function (contexto) {
+    contexto.classList.remove("active");
+  });
+  html.setAttribute("data-contexto", contexto);
+  banner.setAttribute("src", `./imagens/${contexto}.png`);
+  switch (contexto) {
+    case "foco":
+      titulo.innerHTML = `Otimize sua produtividade,<br><strong class="app__title-strong">mergulhe no que importa.</strong>`;
+      break;
+    case "descanso-curto":
+      titulo.innerHTML = `Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
+      break;
+    case "descanso-longo":
+      titulo.innerHTML = `Hora de voltar à superfície. <strong class="app__title-strong">Faça uma pausa longa.</strong>`;
+    default:
+      break;
+  }
 }
