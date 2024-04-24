85dc4d08345ad6ee9529820eb65d3a2b0b0478b0
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-04-24 14:11:57 -0300

    feat: search bar

--- react/codeconnect/src/App.jsx
+++ react/codeconnect/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
+import SearchBar from "./components/SearchBar";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Sidebar />
-    </>
+      <SearchBar />
+    </div>
   );
 }
 
