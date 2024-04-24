0b8f712226d9407251cddceaf9b33eea2200ecc3
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-04-24 14:55:23 -0300

    feat: filter component

---
+++ react/codeconnect/src/components/Filter/index.jsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function Filter() {
+  return (
+    <section className="container-filtro">
+      <ul>
+        <li>Programação</li>
+      </ul>
+      <button>Limpar tudo</button>
+    </section>
+  );
+}
