c93cfbb102a0a74e477ee58ad6c74210159564b5
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-04-25 13:46:59 -0300

    feat: card component

--- react/codeconnect/src/App.jsx
+++ react/codeconnect/src/App.jsx
@@ -1,12 +1,46 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import Card from "./components/Card";
+import Filter from "./components/Filter";
 import SearchBar from "./components/SearchBar";
 import Sidebar from "./components/Sidebar";
 
 function App() {
+  const [dados, setDados] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://my-json-server.typicode.com/MonicaHillman/codeconnect-api/publicacoes"
+    )
+      .then((resposta) => resposta.json())
+      .then((dados) => setDados(dados));
+  }, []);
+
   return (
     <div className="container">
       <Sidebar />
-      <SearchBar />
+      <div>
+        <SearchBar />
+        <Filter />
+        <ul className="lista-cards">
+          {dados
+            ? dados.map((item, index) => (
+                <li key={index}>
+                  <Card
+                    id={item.id}
+                    imagemUrl={item.imagem_capa}
+                    titulo={item.titulo}
+                    resumo={item.resumo}
+                    linhasDeCodigo={item.linhas_de_codigo}
+                    compartilhamentos={item.compartilhamentos}
+                    comentarios={item.comentarios}
+                    usuario={item.usuario}
+                  />
+                </li>
+              ))
+            : null}
+        </ul>
+      </div>
     </div>
   );
 }
