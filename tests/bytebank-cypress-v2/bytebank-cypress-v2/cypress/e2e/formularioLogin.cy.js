4be8a34366ae06faa118d476ada5c439bc725998
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-06-05 15:13:55 -0300

    test: test login with fixtures

--- tests/bytebank-cypress-v2/bytebank-cypress-v2/cypress/e2e/formularioLogin.cy.js
+++ tests/bytebank-cypress-v2/bytebank-cypress-v2/cypress/e2e/formularioLogin.cy.js
@@ -1,4 +1,17 @@
 describe('Formulario de Login', () => {
+  it.only('Deve acessar a página home', () => {
+    cy.fixture('usuarios').then((usuario) => {
+      cy.login(usuario[0].email, usuario[0].senha);
+      cy.visit('/home');
+      cy.url().should('include', '/home');
+      cy.getByData('titulo-boas-vindas').should(
+        'contain',
+        'Bem vindo de volta!'
+      );
+      cy.contains(usuario[0].nome).should('be.visible');
+    });
+  });
+
   it('deve acessar a página home', () => {
     cy.login('neilton@alura.com', '123456');
     cy.visit('/home');
