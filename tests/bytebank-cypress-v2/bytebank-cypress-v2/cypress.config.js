21a69a14db4d9177241273de8dde97debf413e88
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-06-06 13:58:19 -0300

    test: config viewport and test mobile version

--- tests/bytebank-cypress-v2/bytebank-cypress-v2/cypress.config.js
+++ tests/bytebank-cypress-v2/bytebank-cypress-v2/cypress.config.js
@@ -1,7 +1,12 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000'
+    baseUrl: 'http://localhost:3000',
   },
+  env: {
+    mobileViewportWidthBreakpoint: 420,
+  },
+  viewportWidth: 1200,
+  viewportHeight: 900,
 });
