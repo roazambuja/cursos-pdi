55177bd5f470e5bc15cb42db0bbbecceb54361ec
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-06-06 10:42:54 -0300

    test: login with cypress.env

--- tests/bytebank-cypress-v2/bytebank-cypress-v2/cypress/e2e/api/api-users.cy.js
+++ tests/bytebank-cypress-v2/bytebank-cypress-v2/cypress/e2e/api/api-users.cy.js
@@ -88,4 +88,19 @@ describe('Realizando requisições para a API', () => {
       expect(response.body).to.eq('Not Found');
     });
   });
+
+  context('realizando login via API', () => {
+    it('deve permitir o login do usuário Neilton Seguins', () => {
+      cy.request({
+        method: 'POST',
+        url: 'http://localhost:8000/users/login',
+        body: Cypress.env(),
+      }).then((resposta) => {
+        expect(resposta.status).to.be.eq(200);
+        expect(resposta.body).is.not.empty;
+        expect(resposta.body.user).to.have.property('nome');
+        expect(resposta.body.user.nome).to.be.equal('Neilton Seguins');
+      });
+    });
+  });
 });
